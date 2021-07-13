fb74b8cef9620279029cafa98456d5d63cb46079
Hirushani Gamage <chanukshi@gmail.com>
2021-07-13 23:12:53 +0530

    Essay and academicType model class created

---
+++ SchoolManagement.Model/Master/AssessmentType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Model.Master
+{
+    public class AssessmentType
+    {
+        public int AssessmentTypeId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public  bool IsActive { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public  DateTime UpdatedOn { get; set; }
+
+    }
+}
---
+++ SchoolManagement.Model/Master/EssayStudentAnswer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Model.Master
+{
+    class EssayStudentAnswer
+    {
+        public int Id { get; set;}
+        public string TeacherComments { get; set; }
+        public double Marks { get; set; }
+    }
+}
