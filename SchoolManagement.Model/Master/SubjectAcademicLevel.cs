cc4e8e58ce5b4dd99a88aaaa2f81f3b90868f0af
Harshaprabath <prabhathhemasir@gmail.com>
2021-07-14 19:48:47 +0530

    classSubjectTeacher,Subject,SubjectAcademiclevel,SubjectTecher_modle cls

---
+++ SchoolManagement.Model/Master/SubjectAcademicLevel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Model.Master
+{
+    class SubjectAcademicLevel
+    {
+        public int SubjectId { get; set; }
+        public int AcademicLevelId { get; set; }
+       
+
+    }
+}
---
+++ SchoolManagement.Model/Master/SubjectTeacher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Model.Master
+{
+    class SubjectTeacher
+    {
+        public int SubjectTeacherId { get; set; }
+        public int AcademicLevelId { get; set; }
+        public int AcademicYearId { get; set; }
+        public int SubjectId { get; set; }
+        public int TeacherId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public int? CreatedById { get; set; }
+        public DateTime UpdatedOn { get; set; }
+        public int? UpdatedById { get; set; }
+
+     }
+}
