f06e608710fbb8ac7246a44b2af786a5b64bb141
sajitheranda05 <70995241+sajitheranda05@users.noreply.github.com>
2021-07-14 20:37:09 +0530

    Model classes created

---
+++ SchoolManagement.Model/Master/Student.cs
@@ -0,0 +1,21 @@
+﻿//using SchoolManagement.Model.Account;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Model.Master
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public int AdmissionNo { get; set; }
+        public string EmegencyContactNo1 { get; set; }
+        public string? EmegencyContactNo2 { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        //public virtual User UserID { get; set; }
+    }
+}
---
+++ SchoolManagement.Model/Master/StudentClassSubject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Model.Master
+{
+    class StudentClassSubject
+    {
+        public int StudentId { get; set; }
+        public int ClassNameId { get; set; }
+        public int AcademicYearId { get; set; }
+        public int AcademicLevelId { get; set; }
+        public int SubjectId { get; set; }
+
+    }
+}
