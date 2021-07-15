bdcee7c2711930165e07bcd1d96b1681e1a93523
it20022006@my.sliit.lk <it20022006@my.sliit.lk>
2021-07-15 22:41:24 +0530

    LanguageStream and ClassCategory changed to enum folder.

---
+++ SchoolManagement.Util/Enums/Classcategory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Util.Enums
+{
+	public class Classcategory
+    {
+		enum ClassCategory
+		{
+			Maths = 1,
+			Science = 2,
+			Commerce = 3,
+			Technology = 4,
+			Art = 5
+		};
+	}
+}
