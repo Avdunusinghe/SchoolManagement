4d6aaf9a0718c2e500196922a4ca8fccbd9cd9a1
Hirushani Gamage <chanukshi@gmail.com>
2021-07-17 01:26:52 +0530

    config done

--- SchoolManagement.Data/Configurations/Master/AssessmentTypeConfiguration.cs
+++ SchoolManagement.Data/Configurations/Master/AssessmentTypeConfiguration.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SchoolManagement.Data.Common;
+using SchoolManagement.Model.Master;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,19 @@ using System.Threading.Tasks;
 
 namespace SchoolManagement.Data.Configurations.Master
 {
-    public class AssessmentTypeConfiguration
+    public class AssessmentTypeConfiguration : IEntityTypeConfiguration<EssayStudentAnswer>
     {
+        public void Configure(EntityTypeBuilder<EssayStudentAnswer> builder)
+        {
+            builder.ToTable("AssessmentType", Schema.MASTER);
+
+            builder.HasKey(x => x.AssessmentTypeId);
+
+
+            _ = builder.HasMany<AcademicLevelAssessmentType>(ac => ac.AcademicLevelAssesmentType)
+                .WithOne(a => a.AssessmentType)
+                 .HasForeignKey(ac => ac.UpdatedById)
+                  .HasForeignKey(ac => ac.CreatedById);
+        }
     }
 }
--- SchoolManagement.Model/Master/AssessmentType.cs
+++ SchoolManagement.Model/Master/AssessmentType.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SchoolManagement.Model.Account;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,14 +7,21 @@ using System.Threading.Tasks;
 
 namespace SchoolManagement.Model.Master
 {
-    public class AssessmentType
+    public class EssayStudentAnswer
     {
         public int AssessmentTypeId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public  bool IsActive { get; set; }
         public DateTime CreatedOn { get; set; }
+        public int? CreatedById { set; get; }
         public  DateTime UpdatedOn { get; set; }
+        public int? UpdatedById { set; get; }
 
+        public virtual User CreatedBy { set; get; }
+        public virtual User UpdatedBy { set; get; }
+
+        public virtual  ICollection<AcademicLevelAssessmentType> AcademicLevelAssesmentTypes { get; set; }
+        public object EssayStudentAnswers { get; set; }
     }
 }
