87a6bae006eb8db8d768e8b22241e4912e6722bb
sasanka <it20037956@my.sliit.lk>
2021-07-17 05:31:17 +0530

    AcademicLevel modification, AcademicLevel cofigured, AcademicTearConfigured,HeadOfDepartment configured

--- SchoolManagement.Data/Configurations/Master/AcademicLevelConfiguration.cs
+++ SchoolManagement.Data/Configurations/Master/AcademicLevelConfiguration.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SchoolManagement.Data.Common;
+using SchoolManagement.Model.Master;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,13 @@ using System.Threading.Tasks;
 
 namespace SchoolManagement.Data.Configurations.Master
 {
-    public class AcademicLevelConfiguration
+    public class AcademicLevelConfiguration : IEntityTypeConfiguration<AcademicLevel>
     {
+        public void Configure(EntityTypeBuilder<AcademicLevel> builder)
+        {
+            builder.ToTable("AcademicLevel", Schema.MASTER);
+
+            builder.HasKey(x => x.AcademicLevelId);
+        }
     }
 }
--- SchoolManagement.Data/Configurations/Master/AcademicYearConfiguration.cs
+++ SchoolManagement.Data/Configurations/Master/AcademicYearConfiguration.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SchoolManagement.Data.Common;
+using SchoolManagement.Model.Master;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,13 @@ using System.Threading.Tasks;
 
 namespace SchoolManagement.Data.Configurations.Master
 {
-    public class AcademicYearConfiguration
+    public class AcademicYearConfiguration : IEntityTypeConfiguration<AcademicYear>
     {
+        public void Configure(EntityTypeBuilder<AcademicYear> builder)
+        {
+            builder.ToTable("AcademicYear", Schema.MASTER);
+
+            builder.HasKey(x => x.AcademicYearId);
+        }
     }
 }
