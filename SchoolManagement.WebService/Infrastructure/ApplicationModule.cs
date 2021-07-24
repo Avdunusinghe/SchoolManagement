1d78397169a4ec7cc773f69d731266d695a4db17
Ashen Dunusinghe <60811307+Avdunusinghe@users.noreply.github.com>
2021-07-24 11:11:58 +0530

    Web Api startup Configuration completed

---
+++ SchoolManagement.WebService/Infrastructure/ApplicationModule.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+using Microsoft.AspNetCore.Http;
+using SchoolManagement.WebService.Infrastructure.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.WebService.Infrastructure
+{
+    public class ApplicationModule : Autofac.Module
+    {
+        public ApplicationModule()
+        {
+
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<HttpContextAccessor>()
+                .As<IHttpContextAccessor>()
+                .SingleInstance();
+
+            builder.RegisterType<IdentityService>()
+                .As<IIdentityService>()
+                .SingleInstance();
+
+        }
+    }
+}
