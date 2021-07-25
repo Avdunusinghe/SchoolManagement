d427bbf002c67d08daa3df06eac93be995cd6d81
Ashen Dunusinghe <60811307+Avdunusinghe@users.noreply.github.com>
2021-07-25 23:58:49 +0530

    Management Interfaces

---
+++ SchoolManagement.Business/Master/AcademicLevelService.cs
@@ -0,0 +1,13 @@
+﻿using SchoolManagement.Business.Interfaces.MasterData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Business.Master
+{
+    public class AcademicLevelService : IAcademicLevelService
+    {
+    }
+}
