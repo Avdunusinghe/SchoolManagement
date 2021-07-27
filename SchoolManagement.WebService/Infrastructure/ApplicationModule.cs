79620940a624caaeb45f1f7706d909b285fbc7c7
Ashen Dunusinghe <60811307+Avdunusinghe@users.noreply.github.com>
2021-07-27 17:17:54 +0530

    autofac comment

--- SchoolManagement.WebService/Infrastructure/ApplicationModule.cs
+++ SchoolManagement.WebService/Infrastructure/ApplicationModule.cs
@@ -43,119 +43,119 @@ namespace SchoolManagement.WebService.Infrastructure
 
             // Master Services
 
-            builder.RegisterType<AcademicLevelService>()
-               .As<IAcademicLevelService>()
-               .InstancePerLifetimeScope();
+            //builder.RegisterType<AcademicLevelService>()
+            //   .As<IAcademicLevelService>()
+            //   .InstancePerLifetimeScope();
 
-            builder.RegisterType<AcademicYearService>()
-              .As<IAcademicYearService>()
-              .InstancePerLifetimeScope();
+            //builder.RegisterType<AcademicYearService>()
+            //  .As<IAcademicYearService>()
+            //  .InstancePerLifetimeScope();
 
-            builder.RegisterType<ClassNameService>()
-              .As<IClassNameService>()
-              .InstancePerLifetimeScope();
+            //builder.RegisterType<ClassNameService>()
+            //  .As<IClassNameService>()
+            //  .InstancePerLifetimeScope();
 
-            builder.RegisterType<ClassService>()
-              .As<IClassService>()
-              .InstancePerLifetimeScope();
+            //builder.RegisterType<ClassService>()
+            //  .As<IClassService>()
+            //  .InstancePerLifetimeScope();
 
-            builder.RegisterType<ClassService>()
-              .As<IClassService>()
-              .InstancePerLifetimeScope();
+            //builder.RegisterType<ClassService>()
+            //  .As<IClassService>()
+            //  .InstancePerLifetimeScope();
 
-            builder.RegisterType<ClassSubjectTeacher>()
-              .As<IClassSubjectTeacher>()
-              .InstancePerLifetimeScope();
+            //builder.RegisterType<ClassSubjectTeacher>()
+            //  .As<IClassSubjectTeacher>()
+            //  .InstancePerLifetimeScope();
 
-            builder.RegisterType<HeadOfDepartmentService>()
-              .As<IHeadOfDepartmentService>()
-              .InstancePerLifetimeScope();
+            //builder.RegisterType<HeadOfDepartmentService>()
+            //  .As<IHeadOfDepartmentService>()
+            //  .InstancePerLifetimeScope();
 
-            builder.RegisterType<StudentClassService>()
-              .As<IStudentClassService>()
-              .InstancePerLifetimeScope();
+            //builder.RegisterType<StudentClassService>()
+            //  .As<IStudentClassService>()
+            //  .InstancePerLifetimeScope();
 
-            builder.RegisterType<StudentClassSubjectService>()
-              .As<IStudentClassSubjectService>()
-              .InstancePerLifetimeScope();
+            //builder.RegisterType<StudentClassSubjectService>()
+            //  .As<IStudentClassSubjectService>()
+            //  .InstancePerLifetimeScope();
 
-            builder.RegisterType<StudentService>()
-              .As<IStudentService>()
-              .InstancePerLifetimeScope();
+            //builder.RegisterType<StudentService>()
+            //  .As<IStudentService>()
+            //  .InstancePerLifetimeScope();
 
-            builder.RegisterType<SubjectAcademicLevelService>()
-              .As<ISubjectAcademicLevelService>()
-              .InstancePerLifetimeScope();
+            //builder.RegisterType<SubjectAcademicLevelService>()
+            //  .As<ISubjectAcademicLevelService>()
+            //  .InstancePerLifetimeScope();
 
-            builder.RegisterType<SubjectService>()
-              .As<ISubjectService>()
-              .InstancePerLifetimeScope();
+            //builder.RegisterType<SubjectService>()
+            //  .As<ISubjectService>()
+            //  .InstancePerLifetimeScope();
 
-            builder.RegisterType<SubjectStreamService>()
-              .As<ISubjectStreamService>()
-              .InstancePerLifetimeScope();
+            //builder.RegisterType<SubjectStreamService>()
+            //  .As<ISubjectStreamService>()
+            //  .InstancePerLifetimeScope();
 
-            builder.RegisterType<SubjectTeacherService>()
-              .As<ISubjectTeacherService>()
-              .InstancePerLifetimeScope();
+            //builder.RegisterType<SubjectTeacherService>()
+            //  .As<ISubjectTeacherService>()
+            //  .InstancePerLifetimeScope();
 
-            //Lesson Services
+            ////Lesson Services
 
-            builder.RegisterType<EssayQuestionAnswerService>()
-             .As<IEssayQuestionAnswerService>()
-             .InstancePerLifetimeScope();
+            //builder.RegisterType<EssayQuestionAnswerService>()
+            // .As<IEssayQuestionAnswerService>()
+            // .InstancePerLifetimeScope();
 
-            builder.RegisterType<EssayStudentAnswerService>()
-             .As<IEssayStudentAnswerService>()
-             .InstancePerLifetimeScope();
+            //builder.RegisterType<EssayStudentAnswerService>()
+            // .As<IEssayStudentAnswerService>()
+            // .InstancePerLifetimeScope();
 
-            builder.RegisterType<LessonAssignmentService>()
-             .As<ILessonAssignmentService>()
-             .InstancePerLifetimeScope();
+            //builder.RegisterType<LessonAssignmentService>()
+            // .As<ILessonAssignmentService>()
+            // .InstancePerLifetimeScope();
 
-            builder.RegisterType<LessonAssignmentSubmissionService>()
-             .As<ILessonAssignmentSubmissionService>()
-             .InstancePerLifetimeScope();
+            //builder.RegisterType<LessonAssignmentSubmissionService>()
+            // .As<ILessonAssignmentSubmissionService>()
+            // .InstancePerLifetimeScope();
 
-            builder.RegisterType<LessonService>()
-             .As<ILessonService>()
-             .InstancePerLifetimeScope();
+            //builder.RegisterType<LessonService>()
+            // .As<ILessonService>()
+            // .InstancePerLifetimeScope();
 
-            builder.RegisterType<MCQQuestionAnswerService>()
-             .As<IMCQQuestionAnswerService>()
-             .InstancePerLifetimeScope();
+            //builder.RegisterType<MCQQuestionAnswerService>()
+            // .As<IMCQQuestionAnswerService>()
+            // .InstancePerLifetimeScope();
 
-            builder.RegisterType<MCQQuestionStudentAnswerService>()
-             .As<IMCQQuestionStudentAnswerService>()
-             .InstancePerLifetimeScope();
+            //builder.RegisterType<MCQQuestionStudentAnswerService>()
+            // .As<IMCQQuestionStudentAnswerService>()
+            // .InstancePerLifetimeScope();
 
-            builder.RegisterType<QuestionService>()
-             .As<QuestionService>()
-             .InstancePerLifetimeScope();
+            //builder.RegisterType<QuestionService>()
+            // .As<QuestionService>()
+            // .InstancePerLifetimeScope();
 
-            builder.RegisterType<StudentLessonService>()
-             .As<IStudentLessonService>()
-             .InstancePerLifetimeScope();
+            //builder.RegisterType<StudentLessonService>()
+            // .As<IStudentLessonService>()
+            // .InstancePerLifetimeScope();
 
-            builder.RegisterType<StudentLessonTopicContentService>()
-             .As<IStudentLessonTopicContentService>()
-             .InstancePerLifetimeScope();
+            //builder.RegisterType<StudentLessonTopicContentService>()
+            // .As<IStudentLessonTopicContentService>()
+            // .InstancePerLifetimeScope();
 
-            builder.RegisterType<StudentLessonTopicService>()
-             .As<IStudentLessonTopicService>()
-             .InstancePerLifetimeScope();
+            //builder.RegisterType<StudentLessonTopicService>()
+            // .As<IStudentLessonTopicService>()
+            // .InstancePerLifetimeScope();
 
-            builder.RegisterType<StudentMCQQuestionService>()
-             .As<IStudentMCQQuestionService>()
-             .InstancePerLifetimeScope();
+            //builder.RegisterType<StudentMCQQuestionService>()
+            // .As<IStudentMCQQuestionService>()
+            // .InstancePerLifetimeScope();
 
-            builder.RegisterType<TopicContentService>()
-             .As<ITopicContentService>()
-             .InstancePerLifetimeScope();
+            //builder.RegisterType<TopicContentService>()
+            // .As<ITopicContentService>()
+            // .InstancePerLifetimeScope();
 
-            builder.RegisterType<TopicService>()
-             .As<ITopicService>()
-             .InstancePerLifetimeScope();
+            //builder.RegisterType<TopicService>()
+            // .As<ITopicService>()
+            // .InstancePerLifetimeScope();
 
 
         }
