71caa802cabe0a44f7c76d0777b545ca5cb30f28
it20022006@my.sliit.lk <it20022006@my.sliit.lk>
2021-07-28 20:35:25 +0530

    class, classname, classteacher view model created

---
+++ SchoolManagement.ViewModel/Master/ClassTeacherViewModel.cs
@@ -0,0 +1,23 @@
+﻿using SchoolManagement.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.ViewModel.Master
+{
+    class ClassTeacherViewModel
+    {
+		public string ClassNameId { get; set; }
+		public string AcademicLevelId { get; set; }
+		public string AcademicYearId { get; set; }
+		public string TeacherId { get; set; }
+		public bool IsPrimary { get; set; }
+		public bool IsActive { get; set; }
+		public DateTime CreatedOn { get; set; }
+		public int? CreatedById { get; set; }
+		public DateTime UpdatedOn { get; set; }
+		public int? UpdatedById { get; set; }
+	}
+}
