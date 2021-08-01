e7540e854208adf7cdf906aeb318d31c3d620b5c
sasanka <it20037956@my.sliit.lk>
2021-08-02 00:49:12 +0530

    created viewModels for AcademicYear,AcademicLevel,HeadOfDepartment

---
+++ SchoolManagement.Model/Master/HeadOfDepartmentViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Model.Master
+{
+    public class HeadOfDepartmentViewModel
+    {
+        public int Id { get; set; }
+        public int SubjectId { get; set; }
+        public int AcademicLevelId { get; set; }
+        public int AcademicYearId { get; set; }
+        public int TeacherId { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreateOn { get; set; }
+        public int CreatedById { get; set; }
+        public DateTime UpdateOn { get; set; }
+        public int UpdatedById { get; set; }
+    }
+}
