e3f261345b14bb5814e58542f4ce277575e2903b
Ashen Dunusinghe <60811307+Avdunusinghe@users.noreply.github.com>
2021-08-02 00:37:43 +0530

    loggedinuser  service implemeted

---
+++ SchoolManagement.Business/ICurrentUserService.cs
@@ -0,0 +1,14 @@
+﻿using SchoolManagement.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Business
+{
+    public interface ICurrentUserService
+    {
+        User GetUserByUsername(string userName);
+    }
+}
--- SchoolManagement.WebService/Controllers/UserController.cs
+++ SchoolManagement.WebService/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using SchoolManagement.Business.Interfaces.AccountData;
 using SchoolManagement.ViewModel.Account;
+using SchoolManagement.WebService.Infrastructure.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,15 +15,18 @@ namespace SchoolManagement.WebService.Controllers
     public class UserController : ControllerBase
     {
         private readonly IUserService userService;
+        private readonly IIdentityService identityService;
 
-        public UserController(IUserService userService)
+        public UserController(IUserService userService,IIdentityService identityService)
         {
             this.userService = userService;
+            this.identityService = identityService;
         }
 
         [HttpPost]
-        public ActionResult SaveUser(UserViewModel vm, string userName)
+        public async Task<ActionResult> Post([FromBody]UserViewModel vm)
         {
+            var userName = identityService.GetUserName();
             var response = userService.SaveUser(vm, userName);
             return Ok(response);
         }
