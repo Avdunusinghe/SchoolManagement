46fee1e577fe4094ee665c6b3d05c37f72011ba6
sajitheranda05 <70995241+sajitheranda05@users.noreply.github.com>
2021-08-02 16:26:38 +0530

    web service

--- SchoolManagement.Business/Master/StudentService.cs
+++ SchoolManagement.Business/Master/StudentService.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using SchoolManagement.Business.Interfaces.MasterData;
+using SchoolManagement.Data.Data;
+using SchoolManagement.Master.Data.Data;
+using SchoolManagement.ViewModel.Master;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,28 @@ using System.Threading.Tasks;
 
 namespace SchoolManagement.Business.Master
 {
-    public class StudentService
+    public class StudentService : IStudentService
     {
+        private readonly MasterDbContext masterDb;
+        private readonly SchoolManagementContext schoolDb;
+        private readonly IConfiguration config;
+
+        public StudentService(MasterDbContext masterDb, SchoolManagementContext schoolDb, IConfiguration config)
+        {
+            this.masterDb = masterDb;
+            this.schoolDb = schoolDb;
+            this.config = config;
+        }
+
+        public List<StudentViewModel> GetAllStudent()
+        {
+            var response = new List<StudentViewModel>();
+
+            var query = schoolDb.Students;
+
+            var StudentList = query.ToList();
+
+
+        }
     }
 }
