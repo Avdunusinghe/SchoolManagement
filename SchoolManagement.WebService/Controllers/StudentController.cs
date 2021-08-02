10efdae47adb7c135d58e78bb5b03332dbfba2f1
sajitheranda05 <70995241+sajitheranda05@users.noreply.github.com>
2021-08-02 20:51:37 +0530

    Student web service added

---
+++ SchoolManagement.WebService/Controllers/StudentController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SchoolManagement.Business.Interfaces.MasterData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.WebService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly IStudentService studentService;
+
+        public StudentController(IStudentService studentService)
+        {
+            this.studentService = studentService;
+        }
+
+        [HttpGet]
+        public ActionResult GetAllStudents()
+        {
+            var response = studentService.GetAllStudent();
+            return Ok(response);
+        }
+    }
+}
