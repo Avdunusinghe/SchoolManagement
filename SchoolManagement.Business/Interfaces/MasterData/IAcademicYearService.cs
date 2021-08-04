75dcf6bbae31135d0d1f46145f4a87526d18efab
sasanka <it20037956@my.sliit.lk>
2021-08-05 01:58:35 +0530

    AcademicYearServices,HeadOfDepartmentServices,Academic Management controllers created

--- SchoolManagement.Business/Interfaces/MasterData/IAcademicYearService.cs
+++ SchoolManagement.Business/Interfaces/MasterData/IAcademicYearService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SchoolManagement.ViewModel.Master;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace SchoolManagement.Business.Interfaces.MasterData
 {
     public interface IAcademicYearService
     {
+        List<AcademicYearViewModel> GetAllAcademicYear();
     }
 }
---
+++ SchoolManagement.WebService/Controllers/AcademicYearController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SchoolManagement.Business.Interfaces.MasterData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.WebService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AcademicYearController : ControllerBase
+    {
+        private readonly IAcademicYearService AcademicYearService;
+
+        public AcademicYearController(IAcademicYearService AcademicYearService)
+        {
+            this.AcademicYearService = AcademicYearService;
+        }
+
+        [HttpGet]
+        public ActionResult AcademicYear()
+        {
+            var response = AcademicYearService.GetAllAcademicYear();
+            return Ok(response);
+        }
+    }
+}
