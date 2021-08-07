71c9b2fc891fdffba2ec9998ef5d2f53992b1574
it20022006@my.sliit.lk <it20022006@my.sliit.lk>
2021-08-08 00:16:39 +0530

    class and class teacher services added

--- SchoolManagement.Business/Master/ClassTeacherService.cs
+++ SchoolManagement.Business/Master/ClassTeacherService.cs
@@ -19,12 +19,44 @@ namespace SchoolManagement.Business.Master
         private readonly MasterDbContext masterDb;
         private readonly SchoolManagementContext schoolDb;
         private readonly IConfiguration config;
+        private readonly ICurrentUserService currentUserService;
 
-        public ClassTeacherService(MasterDbContext masterDb, SchoolManagementContext schoolDb, IConfiguration config)
+        public ClassTeacherService(MasterDbContext masterDb, SchoolManagementContext schoolDb, IConfiguration config, ICurrentUserService currentUserService)
         {
             this.masterDb = masterDb;
             this.schoolDb = schoolDb;
             this.config = config;
+            this.currentUserService = currentUserService;
+        }
+
+        public List<ClassTeacherViewModel> GetAllClassTeachers()
+        {
+            var response = new List<ClassTeacherViewModel>();
+
+            var query = schoolDb.ClassTeachers.Where(u => u.IsActive == true);
+
+            var ClassTeacherList = query.ToList();
+
+            foreach (var classteacher in ClassTeacherList)
+            {
+                var vm = new ClassTeacherViewModel
+                {
+                    ClassNameId = classteacher.ClassNameId,
+                    AcademicLevelId = classteacher.AcademicLevelId,
+                    AcademicYearId = classteacher.AcademicYearId,
+                    TeacherId = classteacher.TeacherId,
+                    IsPrimary = classteacher.IsPrimary,
+                    IsActive = classteacher.IsActive,
+                    CreatedOn = classteacher.CreatedOn,
+                    CreatedById = classteacher.CreatedById,
+                    UpdatedOn = classteacher.UpdatedOn,
+                    UpdatedById = classteacher.UpdatedById,
+                };
+
+                response.Add(vm);
+            }
+
+            return response;
         }
 
         public async Task <ResponseViewModel> SavaClassTeacher(ClassTeacherViewModel vm, string userName)
@@ -67,6 +99,31 @@ namespace SchoolManagement.Business.Master
                 }
 
                 await schoolDb.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.Message = ex.ToString();
+            }
+
+            return response;
+        }
+
+        public async Task<ResponseViewModel> DeleteClassTeacher(int id)
+        {
+            var response = new ResponseViewModel();
+
+            try
+            {
+                var classTeacher = schoolDb.ClassTeachers.FirstOrDefault(x => x.ClassNameId == id);
+
+                classTeacher.IsActive = false;
+
+                schoolDb.ClassTeachers.Update(classTeacher);
+                await schoolDb.SaveChangesAsync();
+
+                response.IsSuccess = true;
+                response.Message = "Class Teacher Deleted Successfull.";
             }
             catch (Exception ex)
             {
---
+++ SchoolManagement.WebService/Controllers/ClassController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SchoolManagement.Business.Interfaces.MasterData;
+using SchoolManagement.ViewModel.Master;
+using SchoolManagement.WebService.Infrastructure.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.WebService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClassController : ControllerBase
+    {
+        private readonly IClassService classService;
+        private readonly IIdentityService identityService;
+
+        public ClassController(IClassService classService, IIdentityService identityService)
+        {
+            this.classService = classService;
+            this.identityService = identityService;
+        }
+
+        [HttpGet]
+        public ActionResult GetAllClasses()
+        {
+            var response = classService.GetAllClasses();
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] ClassViewModel vm)
+        {
+            var userName = identityService.GetUserName();
+            var response = await classService.SavaClass(vm, userName);
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var response = await classService.DeleteClass(id);
+            return Ok(response);
+        }
+    }
+}
