4346cf542ef3ff46a51ba0ae4235094a0b24d541
sasanka <it20037956@my.sliit.lk>
2021-08-08 22:51:41 +0530

    set method for academic level, delete method for academic year & academic level

--- SchoolManagement.Business/Master/AcademicLevelService.cs
+++ SchoolManagement.Business/Master/AcademicLevelService.cs
@@ -2,6 +2,8 @@
 using SchoolManagement.Business.Interfaces.MasterData;
 using SchoolManagement.Data.Data;
 using SchoolManagement.Master.Data.Data;
+using SchoolManagement.Model;
+using SchoolManagement.ViewModel.Common;
 using SchoolManagement.ViewModel.Master;
 using System;
 using System.Collections.Generic;
@@ -16,19 +18,21 @@ namespace SchoolManagement.Business.Master
         private readonly MasterDbContext masterDb;
         private readonly SchoolManagementContext schoolDb;
         private readonly IConfiguration config;
+        private readonly ICurrentUserService currentUserService;
 
-        public AcademicLevelService(MasterDbContext masterDb, SchoolManagementContext schoolDb, IConfiguration config)
+        public AcademicLevelService(MasterDbContext masterDb, SchoolManagementContext schoolDb, IConfiguration config, ICurrentUserService currentUserService)
         {
             this.masterDb = masterDb;
             this.schoolDb = schoolDb;
             this.config = config;
+            this.currentUserService = currentUserService;
         }
 
         public List<AcademicLevelViewModel> GetAllAcademicLevel()
         {
             var response = new List<AcademicLevelViewModel>();
 
-            var query = schoolDb.AcademicLevels;//.Where(u => u.IsActive == true);//
+            var query = schoolDb.AcademicLevels.Where(al => al.IsActive == true);
 
             var AcademicLevelList = query.ToList();
 
@@ -52,5 +56,82 @@ namespace SchoolManagement.Business.Master
 
             return response;
         }
+
+        public async Task<ResponseViewModel> SaveAcademicLevel(AcademicLevelViewModel academiclevelVM, string userName)
+        {
+            var response = new ResponseViewModel();
+
+            try
+            {
+                var currentuser = currentUserService.GetUserByUsername(userName);
+
+                var academicLevelExist = schoolDb.AcademicLevels.FirstOrDefault(al => al.Id == academiclevelVM.Id);
+
+                if (academicLevelExist == null)
+                {
+                    academicLevelExist = new AcademicLevel()
+                    {
+                        Id = academiclevelVM.Id,
+                        Name = academiclevelVM.Name,
+                        LevelHeadId = academiclevelVM.LevelHeadId,
+                        IsActive = true,
+                        CreatedById = currentuser.Id,
+                        UpdatedOn = DateTime.UtcNow,
+                        UpdatedById = currentuser.Id,
+                    };
+
+                    schoolDb.AcademicLevels.Add(academicLevelExist);
+
+                    response.IsSuccess = true;
+                    response.Message = "Academic Level successfully created";
+                }
+                else
+                {
+                    academicLevelExist.Name = academiclevelVM.Name;
+                    academicLevelExist.LevelHeadId = academiclevelVM.LevelHeadId;
+                    academicLevelExist.IsActive = true;
+                    academicLevelExist.UpdatedById = currentuser.Id;
+                    academicLevelExist.UpdatedOn = DateTime.UtcNow;
+                    
+                    schoolDb.AcademicLevels.Update(academicLevelExist);
+
+                    response.IsSuccess = true;
+                    response.Message = "Academic Level successfully updated";
+                }
+
+                await schoolDb.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.Message = ex.ToString();
+            }
+            return response;
+        }
+
+        public async Task<ResponseViewModel> DeleteAcademicLevel(int id)
+        {
+            var response = new ResponseViewModel();
+
+            try
+            {
+                var academicLevel = schoolDb.AcademicLevels.FirstOrDefault(al => al.Id == id);
+
+                academicLevel.IsActive = false;
+                schoolDb.AcademicLevels.Update(academicLevel);
+                await schoolDb.SaveChangesAsync();
+
+                response.IsSuccess = true;
+                response.Message = "Academic Level successfully deleted";
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.Message = ex.ToString();
+            }
+
+            return response;
+        }
     }
+
 }
--- SchoolManagement.WebService/Controllers/AcademicLevelController.cs
+++ SchoolManagement.WebService/Controllers/AcademicLevelController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SchoolManagement.Business.Interfaces.MasterData;
+using SchoolManagement.ViewModel.Master;
+using SchoolManagement.WebService.Infrastructure.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,16 +15,33 @@ namespace SchoolManagement.WebService.Controllers
     public class AcademicLevelController : ControllerBase
     {
         private readonly IAcademicLevelService AcademicLevelService;
+        private readonly IIdentityService identityService;
 
-        public AcademicLevelController(IAcademicLevelService AcademicLevelService)
+        public AcademicLevelController(IAcademicLevelService AcademicLevelService , IIdentityService identityService)
         {
             this.AcademicLevelService = AcademicLevelService;
+            this.identityService = identityService;
         }
 
         [HttpGet]
         public ActionResult AcademicLevel()
         {
             var response = AcademicLevelService.GetAllAcademicLevel();
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] AcademicLevelViewModel academicLevelVM)
+        {
+            var userName = identityService.GetUserName();
+            var response = await AcademicLevelService.SaveAcademicLevel(academicLevelVM, userName);
+            return Ok(response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var response = await AcademicLevelService.DeleteAcademicLevel(id);
             return Ok(response);
         }
     }
