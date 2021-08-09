58f73d16dac6a2604001d6a4625a203f5e46ca96
Ashen Dunusinghe <60811307+Avdunusinghe@users.noreply.github.com>
2021-08-09 23:57:09 +0530

    class routing

--- clientApp/src/app/admin/class/class-routing.module.ts
+++ clientApp/src/app/admin/class/class-routing.module.ts
@@ -1,12 +1,29 @@
+import { ClassDetailComponent } from './class-detail/class-detail.component';
+import { ClassListComponent } from './class-list/class-list.component';
+import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'classes',
+    pathMatch: 'full',
+  },
+  {
+    path: 'classes',
+    component: ClassListComponent,
+  },
+  {
+    path: 'classes-detail',
+    component: ClassDetailComponent,
+  },
+];
 
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
 export class ClassRoutingModule { }
