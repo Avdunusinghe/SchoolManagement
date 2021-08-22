82f3032f8beb37a49d64a653537f2f3319a8ed98
sasanka <it20037956@my.sliit.lk>
2021-08-22 23:17:50 +0530

    vs code

--- SchoolManagement.Data/Data/SchoolManagementContext.cs
+++ SchoolManagement.Data/Data/SchoolManagementContext.cs
@@ -53,8 +53,8 @@ namespace SchoolManagement.Data.Data
                 //optionsBuilder.UseSqlServer(@"Server=MSI\SQLEXPRESS;Database=SchoolManagement;User Id=ra;Password=1qaz2wsx@;");
                 //optionsBuilder.UseSqlServer(@"Server=DESKTOP-33VQTC5\SQLEXPRESS;Database=SchoolManagement;User Id=cg;Password=1qaz2wsx@;");
                 //optionsBuilder.UseSqlServer(@"Server=LAPTOP-8I5H1L3D;Database=SchoolManagement;User Id=se;Password=1qaz2wsx@;");
-                //optionsBuilder.UseSqlServer(@"Server=LAPTOP-OLUMFMFF\SQLEXPRESS;Database=SchoolManagement;User Id=sc;Password=1qaz2wsx@;");
-                optionsBuilder.UseSqlServer(@"Server=DESKTOP-9KTAG16\SQLEXPRESS;Database=SchoolManagement;User Id=hn;Password=1qaz2wsx@;");
+                optionsBuilder.UseSqlServer(@"Server=LAPTOP-OLUMFMFF\SQLEXPRESS;Database=SchoolManagement;User Id=sc;Password=1qaz2wsx@;");
+                //optionsBuilder.UseSqlServer(@"Server=DESKTOP-9KTAG16\SQLEXPRESS;Database=SchoolManagement;User Id=hn;Password=1qaz2wsx@;");
                 //optionsBuilder.UseSqlServer(@"Server=DESKTOP-JTSNI0P\SQLEXPRESS01;Database=SchoolManagement;User Id=dt;Password=1qaz2wsx@;");
             }
 
