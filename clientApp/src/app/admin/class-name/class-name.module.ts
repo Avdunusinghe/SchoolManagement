c2221a7a8af019529b1a30add972863ef8cebdc0
Hiruni Nawanjana <nawanjuhiru2000@gmail.com>
2021-08-22 15:43:01 +0530

    error fixed

--- clientApp/src/app/admin/class-name/class-name.module.ts
+++ clientApp/src/app/admin/class-name/class-name.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { ClassNameRoutingModule } from './class-name-routing.module';
 import { ClassNameListComponent } from './class-name-list/class-name-list.component';
 import { ClassNameDetailComponent } from './class-name-detail/class-name-detail.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { ToastrModule } from 'ngx-toastr';
 
 
 
@@ -13,7 +16,12 @@ import { ClassNameDetailComponent } from './class-name-detail/class-name-detail.
   ],
   imports: [
     CommonModule,
-    ClassNameRoutingModule
+    ClassNameRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgxDatatableModule,
+    ToastrModule.forRoot(),
+
   ]
 })
 export class ClassNameModule { }
