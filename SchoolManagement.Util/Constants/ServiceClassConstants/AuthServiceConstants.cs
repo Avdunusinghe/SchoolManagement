d1ee36578ebc7218085868e8f376b1678eb3d2ab
Ashen Dunusinghe <60811307+Avdunusinghe@users.noreply.github.com>
2021-08-28 18:23:56 +0530

    auth constants added

--- SchoolManagement.Util/Constants/ServiceClassConstants/AuthServiceConstants.cs
+++ SchoolManagement.Util/Constants/ServiceClassConstants/AuthServiceConstants.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace SchoolManagement.Util.Constants.ServiceClassConstants
 {
-    class AuthServiceConstants
+    public class AuthServiceConstants
     {
+        //Auth service Error Messages
+        public const string AUTH_USER_EXCEPTION_MESSAGE = "User Not Found.";
+        public const string AUTH_FAILED_EXCEPTION_MESSAGE = "Login failed Username or Password Invalid.";
+        public const string AUTH_SCHOOLDOMAIN_INVALID_EXCEPTION_MESSAGE = "Login failed.Invalid school domain name provided.";
+
+
     }
 }
