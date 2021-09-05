5791d75abbc16ff9aeb9b5a99919bab29198f029
Vindy18 <devindip69@gmail.com>
2021-09-05 19:32:32 +0530

    completed crud operations in lesson

--- clientApp/src/app/services/lesson/lesson.service.ts
+++ clientApp/src/app/services/lesson/lesson.service.ts
@@ -1,14 +1,18 @@
 import { environment } from 'src/environments/environment';
 import { LessonModel } from './../../models/lesson/lesson.model';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject,Observable } from 'rxjs';
+import { ResponseModel } from '../../models/common/response.model';
+import {Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { id } from '@swimlane/ngx-datatable';
 ;
 
 @Injectable({
   providedIn: 'root'
 })
 export class LessonService {
+ 
+  
   
   constructor(private httpClient: HttpClient) { }
 
@@ -17,4 +21,18 @@ export class LessonService {
         .get<LessonModel>(environment.apiUrl + '/Lesson' );
   }
 
+  delete(id: number): Observable <ResponseModel> { 
+          return this.httpClient
+            .delete<ResponseModel>(environment.apiUrl + 'lesson/' + id); 
+  }
+
+         
+         
+  saveLesson(vm, LessonModel): Observable <ResponseModel>{
+           return this.httpClient
+              .post<ResponseModel>(environment.apiUrl + 'lesson', vm);
+  
+        
+ 
+  }
 }
--- clientApp/src/app/teacher-home/lesson/lesson-list/lesson-list.component.ts
+++ clientApp/src/app/teacher-home/lesson/lesson-list/lesson-list.component.ts
@@ -1,9 +1,11 @@
+import { Content } from '@angular/compiler/src/render3/r3_ast';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { DatatableComponent } from '@swimlane/ngx-datatable';
 import { ToastrService } from 'ngx-toastr';
 import { LessonModel } from 'src/app/models/lesson/lesson.model';
+import Swal from 'sweetalert2';
 import { LessonService } from './../../../services/lesson/lesson.service';
 
 
@@ -29,12 +31,69 @@ export class LessonListComponent implements OnInit {
     private toastr:ToastrService ) { }
 
   ngOnInit(): void {
+    this.getAllLesson();
+    this.delete(row);
+   
   }
+   //add new lesson using form
+   createNewLesson(content)
+   {
+     this.lessonForm = this.fb.group({
+       Id:['', [Validators.required]],
+       name:['', [Validators.required]],
+       learningoutcome:['', [Validators.required]],
+       planneddate:['', [Validators.required]],
+       completeddate:['', [Validators.required]],
+       status:['', [Validators.required]],
+   });
+ 
+     this.modalService.open(content, {
+       ariaLabelledBy: 'modal-basic-title',
+       size: 'lg',
+     });
+   }
   
-  saveLesson(){
+  saveLesson(content){
+    {
+      this.modalService.open(content, {
+        ariaLabelledBy: 'modal-basic-title',
+        size: 'lg',
+      })
+          
+         
+      
+    }
+  
+   
 
   }
-  deleteLesson(row){
+  delete(row){
+    
+      Swal.fire({
+      title: 'Are you sure to Delete Lesson ?',
+      showCancelButton: true,
+      confirmButtonColor: 'red',
+      cancelButtonColor: 'green',
+      confirmButtonText: 'Yes',
+       }).then((result) => {
+      if (result.value) {
+      this.lessonService.delete(row.id).subscribe(response=>{
+      if(response.isSuccess)
+       {
+      this.toastr.success(response.message,"Success");
+      this.getAllLesson();
+       }
+      else
+       {
+      this.toastr.error(response.message,"Error");
+       }
+       
+       },error=>{
+      this.toastr.error("Network error has been occured. Please try again.","Error");
+       });
+       }
+       });
+       
 
   }
   updateLesson(row:LessonModel,rowIndex:number,content:any){
@@ -45,6 +104,16 @@ export class LessonListComponent implements OnInit {
     });
 
   }
+  getAllLesson(){
+      this.loadingIndicator = true;
+      this.lessonService.getAllLesson().subscribe(response => {
+      this.lesson = response;
+      this.loadingIndicator = false;
+      }, error =>{
+     this.loadingIndicator = false;
+     this.toastr.error("Network error has been occured!, Please try again", "Error")
+      }) 
+     }
 
   addNewLesson(content){
 
@@ -59,3 +128,7 @@ export class LessonListComponent implements OnInit {
     });
   }
 }
+function row(row: any) {
+  throw new Error('Function not implemented.');
+}
+
