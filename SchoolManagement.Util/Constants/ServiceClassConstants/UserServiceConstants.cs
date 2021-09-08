530db0bd2e84fb0afd7b2ae26546588cd540e023
Ashen Dunusinghe <60811307+Avdunusinghe@users.noreply.github.com>
2021-09-08 11:47:58 +0530

    User service backend validation and constant implemented

--- SchoolManagement.Util/Constants/ServiceClassConstants/UserServiceConstants.cs
+++ SchoolManagement.Util/Constants/ServiceClassConstants/UserServiceConstants.cs
@@ -12,6 +12,11 @@ namespace SchoolManagement.Util.Constants
         public const string USER_SAVE_EXCEPTION_MESSAGE = "Error occured. Please try again.";
         public const string EXISTING_USER_DELETE_EXCEPTION_MESSAGE = "Error occured. Please try again.";
 
+        //User service validation Messages
+        public const string EXISTING_USERNAME_ALLREADY_TAKEN_EXCEPTION_MESSAGE = "Username is allready taken";
+        public const string EXISTING_EMAIL_ALLREADY_TAKEN_EXCEPTION_MESSAGE = "Email is allready exist for registered user.";
+
+
         //user service Success Messages
         public const string NEW_USER_SAVE_SUCCESS_MESSAGE = "New user has been added successfully.";
         public const string EXISTING_USER_SAVE_SUCCESS_MESSAGE = "Profile has been updated successfully.";
