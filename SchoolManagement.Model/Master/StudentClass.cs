944a4684c643b90b588997d4c2f9e373109f4a9b
Ashen Dunusinghe <60811307+Avdunusinghe@users.noreply.github.com>
2021-09-12 11:43:14 +0530

    Add Migration Student Class

--- SchoolManagement.Model/Master/StudentClass.cs
+++ SchoolManagement.Model/Master/StudentClass.cs
@@ -13,6 +13,7 @@ namespace SchoolManagement.Model
         public int ClassNameId { get; set; }
         public int AcademicYearId { get; set; }
         public int AcademicLevelId { get; set; }
+        public bool IsActive { get; set; }
 
         public virtual Student Student { get; set; }
         public virtual Class Class { get; set; }
