ed64e58974a05fabf28beaaced640b8237673bc8
Ashen Dunusinghe <60811307+Avdunusinghe@users.noreply.github.com>
2021-09-18 19:10:37 +0530

    Lesson Container added

---
+++ SchoolManagement.ViewModel/Lesson/LessonContainer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.ViewModel.Lesson
+{
+    public class LessonContainer
+    {
+       
+    }
+
+    public class Lesson
+    {
+        public int LessonId { get; set; }
+        public string LessonName { get; set; }
+        public int AcademicYearId { get; set; }
+    }
+    public class LessonObjective
+    {
+        public int Id { get; set; }
+        public string LessonId { get; set; }
+
+    }
+
+}
