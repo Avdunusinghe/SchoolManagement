467f6d2996b1acc7d284cb7944ca28056cbd9161
Ashen Dunusinghe <60811307+Avdunusinghe@users.noreply.github.com>
2021-09-24 19:55:34 +0530

    MasterDataUploadResponse Vm Implemeted

---
+++ SchoolManagement.ViewModel/Common/MasterDataUploadResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.ViewModel.Common
+{
+    public class MasterDataUploadResponse
+    {
+        public MasterDataUploadResponse()
+        {
+            Results = new List<MasterDataFileValidateResult>();
+        }
+        public bool IsSuccess { get; set; }
+        public List<MasterDataFileValidateResult> Results { get; set; }
+    }
+    public class MasterDataFileValidateResult
+    {
+        public bool IsSuccess { get; set; }
+        public string  ValidateMessage { get; set; }
+
+}
+
+
+}
