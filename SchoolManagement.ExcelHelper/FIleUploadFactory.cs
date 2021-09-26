8249369a52dfcf4a5973e2181314ca6fe86f6e04
Ashen Dunusinghe <60811307+Avdunusinghe@users.noreply.github.com>
2021-09-26 09:00:56 +0530

    Factroy Implementation

--- SchoolManagement.ExcelHelper/FIleUploadFactory.cs
+++ SchoolManagement.ExcelHelper/FIleUploadFactory.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using SchoolManagement.Data.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,21 @@ namespace SchoolManagement.ExcelHelper
 {
     public class FIleUploadFactory
     {
+        public BaseExcelMasterDataHelper GetExcelUploader(Dictionary<string, string> helpParams, SchoolManagementContext schoolDb, IConfiguration config)
+        {
+            switch (helpParams["FileType"])
+            {
+                case "User":
+                    {
+                        return new UserExcelMasterDataHelper(helpParams, schoolDb, config);
+                    }
+                case "Student":
+                    {
+                        return new StudentExcelMasterDataHelper(helpParams, schoolDb, config);
+                    }
+            }
+
+            throw new Exception("Unable to find matching excel uploader");
+        }
     }
 }
