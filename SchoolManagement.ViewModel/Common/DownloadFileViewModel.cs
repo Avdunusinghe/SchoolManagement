8de4962b7d084160864f06f337de3c3773960d4e
Ashen Dunusinghe <60811307+Avdunusinghe@users.noreply.github.com>
2021-09-26 08:42:23 +0530

    Vm Updated

--- SchoolManagement.ViewModel/Common/DownloadFileViewModel.cs
+++ SchoolManagement.ViewModel/Common/DownloadFileViewModel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SchoolManagement.ExcelHelper.ExcelUpload.Common
+namespace SchoolManagement.ViewModel.Common
 {
     public class DownloadFileViewModel
     {
