e2988a5be15af6f7ec78c868bf19b8c732f34f28
Ashen Dunusinghe <60811307+Avdunusinghe@users.noreply.github.com>
2021-09-26 08:40:06 +0530

    Excel Upload Library Intalled

--- SchoolManagement.ExcelHelper/ExcelUpload/BaseExcelMasterDataHelper.cs
+++ SchoolManagement.ExcelHelper/ExcelUpload/BaseExcelMasterDataHelper.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using SchoolManagement.Data.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,15 @@ namespace SchoolManagement.ExcelHelper
 {
     public class BaseExcelMasterDataHelper
     {
+        protected readonly Dictionary<string, string> helpParams;
+        protected readonly SchoolManagementContext schoolDb;
+        protected readonly IConfiguration config;
+
+        public BaseExcelMasterDataHelper(Dictionary<string, string> helpParams, SchoolManagementContext schoolDb, IConfiguration config)
+        {
+            this.helpParams = helpParams;
+            this.schoolDb = schoolDb;
+            this.config = config;
+        }
     }
 }
