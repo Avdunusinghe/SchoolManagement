e623549a0d7799329f6eeceae85fac31d4ce4a38
Ashen Dunusinghe <60811307+Avdunusinghe@users.noreply.github.com>
2021-09-30 13:34:29 +0530

    Lesson Contenet ViewModel Implemeted

---
+++ clientApp/src/app/teacher-home/lesson/lesson-container/lesson-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lesson-container',
+  templateUrl: './lesson-container.component.html',
+  styleUrls: ['./lesson-container.component.sass']
+})
+export class LessonContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- clientApp/src/app/teacher-home/lesson/lesson.module.ts
+++ clientApp/src/app/teacher-home/lesson/lesson.module.ts
@@ -1,3 +1,6 @@
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import { TableModule } from 'primeng/table';
+import { MultiSelectModule } from 'primeng/multiselect';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LessonRoutingModule } from './lesson-routing.module';
@@ -8,22 +11,34 @@ import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import { ToastrModule } from 'ngx-toastr';
 import { CalendarModule } from 'primeng/calendar';
 import{LessonContentComponent} from './lesson-content/lesson-content.component'
-
-
+import { NgbProgressbarModule } from '@ng-bootstrap/ng-bootstrap';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import {ListboxModule} from 'primeng/listbox';
+import { ButtonModule } from 'primeng/button';
+import { LessonContainerComponent } from './lesson-container/lesson-container.component';
 
 @NgModule({
   declarations: [
     LessonDetailComponent,
     LessonListComponent,
-    LessonContentComponent
+    LessonContentComponent,
+    LessonContainerComponent
   ],
   imports: [
     CommonModule,
     LessonRoutingModule,
+    CommonModule,
+    NgbProgressbarModule,
     FormsModule,
+    MultiSelectModule,
     ReactiveFormsModule,
-    NgxDatatableModule,
     CalendarModule,
+    InputSwitchModule,
+    TableModule,
+    ListboxModule,
+    ButtonModule,
+    CKEditorModule,
+    NgxDatatableModule,
     ToastrModule.forRoot(),
   ]
 })
