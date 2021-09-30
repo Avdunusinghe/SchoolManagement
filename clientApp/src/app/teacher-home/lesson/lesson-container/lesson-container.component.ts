31fc0d562ca799437663d19e2a3194cb4fc2ccc7
Ashen Dunusinghe <60811307+Avdunusinghe@users.noreply.github.com>
2021-09-30 13:48:51 +0530

    Lesson Container Implemeted

--- clientApp/src/app/teacher-home/lesson/lesson-container/lesson-container.component.ts
+++ clientApp/src/app/teacher-home/lesson/lesson-container/lesson-container.component.ts
@@ -1,4 +1,10 @@
+import { LessonService } from './../../../services/lesson/lesson.service';
+import { PrimeNGConfig } from 'primeng/api';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { LessonModel } from './../../../models/lesson/lesson.model';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-lesson-container',
@@ -7,9 +13,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LessonContainerComponent implements OnInit {
 
-  constructor() { }
+  menuItems: LessonMenu[];
+  selectedMenu:LessonMenu;
+  lessonId:number=0;
+  lesson:LessonModel = new LessonModel();
+  lessonForm:FormGroup;
+
+  isDisable:boolean=false;
+
+  constructor( 
+    public activateRoute: ActivatedRoute,
+    private spinner: NgxSpinnerService,
+    private primengConfig: PrimeNGConfig,
+    private lessonService:LessonService,
+    private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+
+  createLessonForm():FormGroup {
+    return new FormGroup({
+   
+        id: new FormControl(0),
+        lessonDetail: this.fb.group({
+        lessonId:new FormControl(0),
+        name:new FormControl(""),
+        lessonIntroduction: new FormControl(""),
+        academicYearId: new FormControl(null,[Validators.required]),
+        lessonStatus:new FormControl(null,[Validators.required]),
+        teacherAids:new FormControl([]),
+        assignedClasses:new FormControl([]),
+      }),
+     
+      lessonTopics: this.fb.array([]),
+      
+    });
+  }
+
+}
+interface LessonMenu {
+  name: string,
+  code: string,
+  id:number
 }
