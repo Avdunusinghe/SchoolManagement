12dc97eb149da072c04f99662e1732e4be671cad
Hirushani Gamage <chanukshi@gmail.com>
2021-10-04 17:22:16 +0530

    Report generate implemented

---
+++ SchoolManagement.ViewModel/Report/EssayAnswerStudentReport.cs
@@ -0,0 +1,139 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.ViewModel.Report
+{
+    public class EssayAnswerStudentReport
+    {
+        #region Declaration
+        int _totalColumn = 4;
+        Document _document;
+        iTextSharp.text.Font _fontStyle;
+        iTextSharp.text.pdf.PdfPTable _pdfPTable = new PdfPTable(4);
+        iTextSharp.text.pdf.PdfPCell _pdfPCell;
+        MemoryStream _memoryStream = new MemoryStream();
+        List<EssayStudentAnswerReportViewModel> _essaystudentanswers = new List<EssayStudentAnswerReportViewModel>();
+        #endregion
+
+        public byte[] PrepareReport(List<EssayStudentAnswerReportViewModel> response)
+        {
+            _essaystudentanswers = response;
+
+            #region
+            _document = new Document(PageSize.A4, 0f, 0f, 0f, 0f);
+            _document.SetPageSize(PageSize.A4);
+            _document.SetMargins(20f, 20f, 20f, 20f);
+            _pdfPTable.WidthPercentage = 100;
+            _pdfPTable.HorizontalAlignment = Element.ALIGN_LEFT;
+            _fontStyle = FontFactory.GetFont("TimesNewRoman", 8f, 1);
+
+            iTextSharp.text.pdf.PdfWriter.GetInstance(_document, _memoryStream);
+            _document.Open();
+            _pdfPTable.SetWidths(new float[] { 80f, 150f, 100f ,100f});
+            #endregion
+
+            this.ReportHeader();
+            this.ReportBody();
+            _pdfPTable.HeaderRows = 4;
+            _document.Add(_pdfPTable);
+            _document.Close();
+            return _memoryStream.ToArray();
+
+        }
+
+        private void ReportHeader()
+        {
+            _fontStyle = FontFactory.GetFont("TimesNewRoman", 18f, 1);
+            _pdfPCell = new PdfPCell(new Phrase("STUDENT MARKS REPORT", _fontStyle));
+            _pdfPCell.Colspan = _totalColumn;
+            _pdfPCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfPCell.Border = 0;
+            _pdfPCell.BackgroundColor = BaseColor.WHITE;
+            _pdfPCell.ExtraParagraphSpace = 7;
+            _pdfPTable.AddCell(_pdfPCell);
+            _pdfPTable.CompleteRow();
+
+            _fontStyle = FontFactory.GetFont("TimesNewRoman", 12f, 1);
+            _pdfPCell = new PdfPCell(new Phrase("STUDENT ESSAY MARKS LIST", _fontStyle));
+            _pdfPCell.Colspan = _totalColumn;
+            _pdfPCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfPCell.Border = 0;
+            _pdfPCell.BackgroundColor = BaseColor.WHITE;
+            _pdfPCell.ExtraParagraphSpace = 7;
+            _pdfPTable.AddCell(_pdfPCell);
+            _pdfPTable.CompleteRow();
+        }
+
+        private void ReportBody()
+        {
+            #region Table header
+            _fontStyle = FontFactory.GetFont("TimesNewRoman", 10f, 1);
+            _pdfPCell = new PdfPCell(new Phrase("QUESTION ID", _fontStyle));
+            _pdfPCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfPCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            _pdfPCell.BackgroundColor = BaseColor.LIGHT_GRAY;
+            _pdfPTable.AddCell(_pdfPCell);
+
+
+            _pdfPCell = new PdfPCell(new Phrase("STUDENT NAME", _fontStyle));
+            _pdfPCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfPCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            _pdfPCell.BackgroundColor = BaseColor.LIGHT_GRAY;
+            _pdfPTable.AddCell(_pdfPCell);
+
+            _pdfPCell = new PdfPCell(new Phrase("MARKS", _fontStyle));
+            _pdfPCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfPCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            _pdfPCell.BackgroundColor = BaseColor.LIGHT_GRAY;
+            _pdfPTable.AddCell(_pdfPCell);
+
+            _pdfPCell = new PdfPCell(new Phrase("TEACHER COMMENTS", _fontStyle));
+            _pdfPCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfPCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            _pdfPCell.BackgroundColor = BaseColor.LIGHT_GRAY;
+            _pdfPTable.AddCell(_pdfPCell);
+            _pdfPTable.CompleteRow();
+            #endregion
+
+            #region Table Body
+            _fontStyle = FontFactory.GetFont("TimesNewRoman", 10f, 0);
+            foreach (EssayStudentAnswerReportViewModel vm in _essaystudentanswers)
+            {
+                _pdfPCell = new PdfPCell(new Phrase(vm.QuestionId.ToString(), _fontStyle));
+                _pdfPCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                _pdfPCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+               // _pdfPCell.BackgroundColor = BaseColor.LIGHT_GRAY;
+                _pdfPTable.AddCell(_pdfPCell);
+
+                _pdfPCell = new PdfPCell(new Phrase(vm.StudentName, _fontStyle));
+                _pdfPCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                _pdfPCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                //_pdfPCell.BackgroundColor = BaseColor.LIGHT_GRAY;
+                _pdfPTable.AddCell(_pdfPCell);
+
+                _pdfPCell = new PdfPCell(new Phrase(vm.Marks.ToString(), _fontStyle));
+                _pdfPCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                _pdfPCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+               // _pdfPCell.BackgroundColor = BaseColor.LIGHT_GRAY;
+                _pdfPTable.AddCell(_pdfPCell);
+
+                _pdfPCell = new PdfPCell(new Phrase(vm.TeacherComments, _fontStyle));
+                _pdfPCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                _pdfPCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                //_pdfPCell.BackgroundColor = BaseColor.LIGHT_GRAY;
+                _pdfPTable.AddCell(_pdfPCell);
+                _pdfPTable.CompleteRow();
+            }
+            #endregion
+
+        }
+
+
+    }
+}
