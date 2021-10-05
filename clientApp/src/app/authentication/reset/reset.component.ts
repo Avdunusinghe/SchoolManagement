c9a60ea748f10e49fccf6e361f4f7d6c6e807bb5
Ashen Dunusinghe <60811307+Avdunusinghe@users.noreply.github.com>
2021-10-05 15:08:20 +0530

    Reset Password View Implemeted

--- clientApp/src/app/authentication/reset/reset.component.ts
+++ clientApp/src/app/authentication/reset/reset.component.ts
@@ -1,15 +1,36 @@
+import { MessageService } from 'primeng/api';
+import { AuthService } from 'src/app/core/service/auth.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-reset',
   templateUrl: './reset.component.html',
-  styleUrls: ['./reset.component.sass']
+  styleUrls: ['./reset.component.sass'],
+  providers: [MessageService]
 })
 export class ResetComponent implements OnInit {
 
-  constructor() { }
+  chanagePasswordForm:FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private messageService: MessageService
+  ) { }
 
   ngOnInit(): void {
+    this.chanagePasswordForm = this.formBuilder.group({
+      schoolDomain:['', Validators.required],
+      password:['', Validators.required],
+      confirmPassword:['', Validators.required]
+    });
   }
+
+  get f() {
+    return this.chanagePasswordForm.controls;
+  }
+
+  onSubmit() {}
 
 }
